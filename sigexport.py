9ddaa25592321b6ee29d3fe1430d0d9c3b5e2b34
Chris Arderne <chris@rdrn.me>
2019-05-30 08:19:18 +0200

    Refactor SQLite code

--- sigexport.py
+++ sigexport.py
@@ -83,12 +83,70 @@ def make_simple(dst, conversations, contacts):
                 print(f"[{date}] {sender} : {body}", file=f)
 
 
+def fetch_data(db_file, key):
+    """Load SQLite data into dicts."""
+
+    contacts = {}
+    convos = {}
+
+    db = sqlcipher.connect(str(db_file))
+    c = db.cursor()
+    c2 = db.cursor()
+
+    # param binding doesn't work for pragmas, so use a direct string concat
+    for cursor in [c, c2]:
+        cursor.execute(f"PRAGMA KEY = \"x'{key}'\"")
+        cursor.execute(f"PRAGMA cipher_page_size = 1024")
+        cursor.execute(f"PRAGMA kdf_iter = 64000")
+        cursor.execute(f"PRAGMA cipher_hmac_algorithm = HMAC_SHA1")
+        cursor.execute(f"PRAGMA cipher_kdf_algorithm = PBKDF2_HMAC_SHA1")
+
+    c.execute("SELECT json, id, name, profileName, type, members FROM conversations")
+    for result in c:
+        cId = result[1]
+        isGroup = result[4] == "group"
+        contacts[cId] = {
+            "id": result[1],
+            "name": result[2],
+            "profileName": result[3],
+            "isGroup": isGroup,
+        }
+        convos[cId] = []
+
+        if isGroup:
+            usableMembers = []
+            # Match group members from ID (phone number) back to real
+            for member in result[5].split():
+                c2.execute(
+                    "SELECT name, profileName FROM conversations WHERE id=?", [member]
+                )
+                for name in c2:
+                    useName = name[0] if name else member
+                    usableMembers.append(useName)
+
+            contacts[cId]["members"] = usableMembers
+
+    c.execute(
+        "SELECT json, conversationId, sent_at, received_at FROM messages ORDER BY sent_at"
+    )
+    messages = []
+    for result in c:
+        content = json.loads(result[0])
+        cId = result[1]
+        if not cId:
+            # Signal's data model isn't as stable as one would imagine
+            continue
+        convos[cId].append(content)
+
+    return convos, contacts
+
+
 @click.command()
 @click.option(
     "--config", "-c", type=click.Path(), help="Path to Signal config and database"
 )
 @click.argument("dst", type=click.Path(), default="output")
-def export(dst, config=None):
+def main(dst, config=None):
     """
     Read the Signal directory and output attachments and chat files to DST directory.
     Assumes the following default directories, can be over-ridden wtih --config.
@@ -108,76 +166,21 @@ def export(dst, config=None):
     db_file = src / "sql" / "db.sqlite"
 
     dst = Path(dst).expanduser()
-    if dst.is_dir():
+    if not dst.is_dir():
+        dst.mkdir(parents=True)
+    else:
         print("Output folder already exists, didn't do anything!")
         sys.exit(1)
-    dst.mkdir(parents=True)
 
     # Read sqlcipher key from Signal config file
-    try:
+    if config.is_file():
         with open(config, "r") as conf:
             key = json.loads(conf.read())["key"]
-    except FileNotFoundError:
+    else:
         print(f"Error: {config} not found in directory {src}")
         sys.exit(1)
 
-    db = sqlcipher.connect(str(db_file))
-    c = db.cursor()
-    c2 = db.cursor()
-
-    # param binding doesn't work for pragmas, so use a direct string concat
-    for cursor in [c, c2]:
-        cursor.execute(f"PRAGMA KEY = \"x'{key}'\"")
-        cursor.execute(f"PRAGMA cipher_page_size = 1024")
-        cursor.execute(f"PRAGMA kdf_iter = 64000")
-        cursor.execute(f"PRAGMA cipher_hmac_algorithm = HMAC_SHA1")
-        cursor.execute(f"PRAGMA cipher_kdf_algorithm = PBKDF2_HMAC_SHA1")
-
-    contacts = {}
-    convos = {}
-
-    c.execute("SELECT json, id, name, profileName, type, members FROM conversations")
-    for result in c:
-        cId = result[1]
-        isGroup = result[4] == "group"
-        contacts[cId] = {
-            "id": result[1],
-            "name": result[2],
-            "profileName": result[3],
-            "isGroup": isGroup,
-        }
-        convos[cId] = []
-
-        if isGroup:
-            usableMembers = []
-            # Attempt to match group members from ID (phone number) back to real
-            # names if the real names are also in your contact/conversation list.
-            for member in result[5].split():
-                c2.execute(
-                    "SELECT name, profileName FROM conversations WHERE id=?", [member]
-                )
-                for name in c2:
-                    useName = name[0] if name else member
-                    usableMembers.append(useName)
-
-            contacts[cId]["members"] = usableMembers
-
-    # We either need an ORDER BY or a manual sort() below because our web interface
-    # processes message history in array order with javascript object traversal.
-    # If we skip ordering here, the web interface will show the message history in
-    # a random order, which can be amusing but not necessarily very useful.
-    c.execute(
-        "SELECT json, conversationId, sent_at, received_at FROM messages ORDER BY sent_at"
-    )
-    messages = []
-    for result in c:
-        content = json.loads(result[0])
-        cId = result[1]
-        if not cId:
-            # Signal's data model isn't as stable as one would imagine
-            continue
-        convos[cId].append(content)
-
+    convos, contacts = fetch_data(db_file, key)
     convos = copy_attachments(src, dst, convos, contacts)
     make_simple(dst, convos, contacts)
 
@@ -185,4 +188,4 @@ def export(dst, config=None):
 
 
 if __name__ == "__main__":
-    export()
+    main()
