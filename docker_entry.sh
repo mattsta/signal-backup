7221d4ed73f51ef8348cd974bb648cd74437e88e
Chris Arderne <chris@rdrn.me>
2022-01-05 14:15:19 +0200

    Add seconds to docker_entry

--- docker_entry.sh
+++ docker_entry.sh
@@ -3,7 +3,7 @@
 set -e
 trap "{ exit 0; }" TERM INT
 
-OUTPUT_SUBDIR=`date +%Y-%m-%dT%H-%M`
+OUTPUT_SUBDIR=`date +%Y-%m-%dT%H-%M-%S`
 
 sigexport --source=/Signal /output/$OUTPUT_SUBDIR "$@"
 chown -R dummy /output/
